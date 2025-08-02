f77eb7ac94f200dbecbf915c05ae63f33f015084
ishan <ishansingla.zy@gmail.com>
2025-08-02 13:34:56 +0530

    test

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ from middleware.logMiddleware import discord_webhook_middleware
 from starlette.middleware.base import BaseHTTPMiddleware
 
 app = FastAPI()
-app.include_router(router)
-app.add_middleware(BaseHTTPMiddleware, dispatch=discord_webhook_middleware)
-app.add_middleware(BaseHTTPMiddleware, dispatch=authentication_middleware)
-
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the HackRX API"}
 tags=["hackrx"]
+app.include_router(router)
+app.add_middleware(BaseHTTPMiddleware, dispatch=discord_webhook_middleware)
+app.add_middleware(BaseHTTPMiddleware, dispatch=authentication_middleware)
